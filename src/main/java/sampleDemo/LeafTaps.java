60e32d8a53dc127738f240777ccd5b3050f86856
Gopinath Jayakumar <40163531+GopinathJayakumar@users.noreply.github.com>
2021-04-04 14:47:15 +0530

    Update LeafTaps.java

--- src/main/java/sampleDemo/LeafTaps.java
+++ src/main/java/sampleDemo/LeafTaps.java
@@ -16,7 +16,7 @@ public class LeafTaps {
 		driver.get("http://leaftaps.com/opentaps");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		driver.findElementById("username1").sendKeys("DemoSalesManager");
+		driver.findElementById("username").sendKeys("DemoSalesManager");
 		driver.findElementById("password").sendKeys("crmsfa");
 		driver.findElementByClassName("decorativeSubmit").click();
 		driver.findElementByClassName("decorativeSubmit").click();
